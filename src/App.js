a0a815d2bf511d8d71c2ad2353cdaf11db1cc1a3
harshnambiar <harrisonlvl2@gmail.com>
2022-10-14 13:42:32 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import {authenticate, unauthenticate} from './index.js';
 
 function App() {
   return (
     <div className="App">
+      <div className='wallet' align="left">
+
+        <div className="button" onClick={authenticate}>Sign In!</div>
+        <div className="button" onClick={unauthenticate}>Sign Out!</div>
+        </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://dev.to/maxstalker"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to the World of FLOW!
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as fcl from "@onflow/fcl";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,34 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+export function authenticate(){
+  fcl.config()
+  // connect to Flow testnet
+  // for fcl@<1.0.0 this should be https://access-testnet.onflow.org
+    .put("accessNode.api", "https://rest-testnet.onflow.org")
+    // use pop instead of default IFRAME/RPC option for security enforcement
+    .put("discovery.wallet.method", "POP/RPC")
+    
+    // use Blocto testnet wallet
+    .put("challenge.handshake", "https://flow-wallet-testnet.blocto.app/authn")
+
+    fcl
+      .currentUser()
+      .subscribe(console.log) // fires everytime account connection status updates
+      
+    // authenticate
+    fcl.authenticate()
+}
+
+export function unauthenticate(){
+    fcl
+      .currentUser()
+      .subscribe(console.log) // fires everytime account connection status updates
+    
+  // unauthenticate and clear account info in FCL
+  fcl.unauthenticate()
+}
